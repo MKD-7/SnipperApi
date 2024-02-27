0d41b10f1cf5d7cdfda3a7a2c84a23477f3d0257
Marcus Dedi <Marcus.Dedi@shawbrook.co.uk>
2024-02-27 11:24:18 +0000

    All snippets are set to be encrypting when creating and se to be decrypted when retrived.

--- SnipperAPI/Controllers/SnippetsControllers.cs
+++ SnipperAPI/Controllers/SnippetsControllers.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
 
 namespace SnippetApi.Controllers
 {
@@ -9,7 +15,7 @@ namespace SnippetApi.Controllers
         private static readonly List<Snippet> Snippets = new List<Snippet>();
 
         // POST api/snippets
-        [HttpPost]
+        /*[HttpPost]
         public ActionResult<Snippet> CreateSnippet([FromBody] SnippetRequest snippetRequest)
         {
             // Validating incoming data
@@ -26,10 +32,40 @@ namespace SnippetApi.Controllers
 
             // Returning the created snippet with Created status - use abstraction / restfulAPI
             return CreatedAtAction(nameof(GetSnippet), new { id = newSnippet.Id }, newSnippet);
-        }
+        }*/
 
-        // GET api/snippets/{id}
-        [HttpGet("{id}")]
+
+        [HttpPost("snippets")]
+        [Authorize] // Requires authentication
+        public ActionResult<Snippet> CreateSnippet([FromBody] SnippetRequest snippetRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                var currentUser = _userManager.GetUserAsync(User).Result;
+
+                // Encrypt the content before saving it
+                var encryptedContent = EncryptionHelper.Encrypt(snippetRequest.Content);
+
+                var newSnippet = new Snippet
+                {
+                    Id = Guid.NewGuid(),
+                    Title = snippetRequest.Title,
+                    Content = encryptedContent, // Save the encrypted content
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                Snippets.Add(newSnippet);
+
+                return CreatedAtAction(nameof(GetSnippet), new { id = newSnippet.Id }, newSnippet);
+            }
+
+            return BadRequest("Invalid snippet data.");
+        }
+    }
+
+
+    //GET api/snippets/{id}
+    [HttpGet("{id}")]
         public ActionResult<Snippet> GetSnippet(Guid id)
         {
             // Find the snippet by Id
@@ -47,7 +83,7 @@ namespace SnippetApi.Controllers
 
         //create endpoint for all snippets 
 
-        [HttpGet("snippets")]
+        /*[HttpGet("snippets")]
         public ActionResult<IEnumerable<Snippet>> GetAllSnippets()
         {
             // Assuming Snippets is a list of Snippet objects in your data store
@@ -57,7 +93,37 @@ namespace SnippetApi.Controllers
             }
 
             return Ok(Snippets);
+        }*/
+
+
+        [HttpGet("snippets")]
+        [Authorize] // Requires authentication
+        public ActionResult<IEnumerable<Snippet>> GetAllSnippets()
+        {
+            // Get the current user
+            var currentUser = _userManager.GetUserAsync(User).Result;
+
+            //  list of Snippet objects in your data store
+            if (Snippets.Count == 0)
+            {
+                return NotFound("No snippets found.");
+            }
+
+            // Decrypt snippets before returning
+            var decryptedSnippets = Snippets.Select(snippet =>
+                new Snippet
+                {
+                    Id = snippet.Id,
+                    Title = snippet.Title,
+                    Content = EncryptionHelper.Decrypt(snippet.Content), // Decrypt the content
+                    CreatedAt = snippet.CreatedAt
+                }
+            );
+
+            return Ok(decryptedSnippets);
         }
+
+
 
 
         // retrive by query 
